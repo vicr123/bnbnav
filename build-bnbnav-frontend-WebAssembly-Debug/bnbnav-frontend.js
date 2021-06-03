e883f17c4027d8a9c5e5fde370d0cea64cc391d2
Victor Tran <vicr12345@gmail.com>
2021-06-03 20:42:38 +1000

    Add translation files

--- build-bnbnav-frontend-WebAssembly-Debug/bnbnav-frontend.js
+++ build-bnbnav-frontend-WebAssembly-Debug/bnbnav-frontend.js
@@ -691,8 +691,8 @@ var wasmMemory;
 // In the wasm backend, we polyfill the WebAssembly object,
 // so this creates a (non-native-wasm) table for us.
 var wasmTable = new WebAssembly.Table({
-  'initial': 10869,
-  'maximum': 10869 + 0,
+  'initial': 10883,
+  'maximum': 10883 + 0,
   'element': 'anyfunc'
 });
 
@@ -1293,11 +1293,11 @@ function updateGlobalBufferAndViews(buf) {
 }
 
 var STATIC_BASE = 1024,
-    STACK_BASE = 9715504,
+    STACK_BASE = 9726128,
     STACKTOP = STACK_BASE,
-    STACK_MAX = 4472624,
-    DYNAMIC_BASE = 9715504,
-    DYNAMICTOP_PTR = 4472448;
+    STACK_MAX = 4483248,
+    DYNAMIC_BASE = 9726128,
+    DYNAMICTOP_PTR = 4483072;
 
 assert(STACK_BASE % 16 === 0, 'stack must start aligned');
 assert(DYNAMIC_BASE % 16 === 0, 'heap must start aligned');
@@ -1832,7 +1832,7 @@ function say(text,lang){ text = UTF8ToString(text); lang = UTF8ToString(lang); l
 
 
 
-// STATICTOP = STATIC_BASE + 4471600;
+// STATICTOP = STATIC_BASE + 4482224;
 /* global initializers */  __ATINIT__.push({ func: function() { ___wasm_call_ctors() } });
 
 
@@ -8306,7 +8306,7 @@ function say(text,lang){ text = UTF8ToString(text); lang = UTF8ToString(lang); l
     }
 
   function _emscripten_get_sbrk_ptr() {
-      return 4472448;
+      return 4483072;
     }
 
   
@@ -12675,7 +12675,7 @@ function say(text,lang){ text = UTF8ToString(text); lang = UTF8ToString(lang); l
   }
   
   
-  var _fetch_work_queue=4472608;function __emscripten_get_fetch_work_queue() {
+  var _fetch_work_queue=4483232;function __emscripten_get_fetch_work_queue() {
       return _fetch_work_queue;
     }function _emscripten_start_fetch(fetch, successcb, errorcb, progresscb, readystatechangecb) {
     if (typeof noExitRuntime !== 'undefined') noExitRuntime = true; // If we are the main Emscripten runtime, we should not be closing down.
@@ -13295,10 +13295,10 @@ function say(text,lang){ text = UTF8ToString(text); lang = UTF8ToString(lang); l
     }
 
   
-  var ___tm_current=4472464;
+  var ___tm_current=4483088;
   
   
-  var ___tm_timezone=(stringToUTF8("GMT", 4472512, 4), 4472512);
+  var ___tm_timezone=(stringToUTF8("GMT", 4483136, 4), 4483136);
   
   function _tzset() {
       // TODO: Use (malleable) environment variables instead of system settings.
